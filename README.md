002243e1ef18bc6a47cacf150fa9937a6b9c74a1
Shawn Huckabay <shawn.huckabay@gmail.com>
2017-08-23 15:58:31 -0500

    Added new actions and expanded functionality

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # do-spaces
 
-This cookbook adds a do_space resource you can use to sync the content of
-a DigitalOcean Space to your local filesystem or vice versa.
+This cookbook adds a do_space resource you can use to sync the content of a DigitalOcean Space to your local filesystem or vice versa.
 
-A basic example, which will sync the content of the directory '/web' in the
-DigitalOcean Space 'my-space' to the local directory '/website'
+A basic example, which will create the Space 'my-space' if it doesn't exist, and sync the content of the local directory '/website' to the Space directory '/web'.
 
 ```
 do_space 'my-space' do
   local_path '/website'
   remote_path '/web'
-  action :sync_local_dir
+  action [:create, :sync_remote_dir]
   key 'ABC123DEF456GHI789JK'
   secret_key 'abc123DEF456ghi789JKL012mno345PQR678stu901v'
   region 'nyc3'
@@ -30,25 +28,31 @@ Debian 8 and above
 Fedora 25 and above
 
 CentOS 7
-* Note: CentOS support requires installing some RPMs meant for Fedora 26. These will be installed automatically when using this recipe. This is because the version of s3cmd built into CentOS 7 doesn't seem to work correctly.
+* Note: CentOS support requires installing some RPMs meant for Fedora 26. These will be installed automatically when using this recipe. This is because the version of s3cmd built into CentOS 7 doesn't seem to work correctly. Use with caution.
 
 ## Actions
 
 Two actions are available:
-* sync_local_dir (default): Syncs content from a DigitalOcean Space to a local directory.
-* sync_remote_dir: Syncs content from a local directory to a DigitalOcean Space.
+* create: Creates a new private Space unless public_space is set to true, in which case, it creates a new public Space. If the Space already exists, updates the permissions of the Space based on the public_space property, while leaving permissions alone if public_space is set to its default value of nil.
+* update: Updates the permissions of the Space based on the public_space property, but does nothing if public_space property is set to its default value of nil. Generates an error if the Space does not exist and the public_space property is set to true or false.
+* delete: Deletes an existing Space. Continues without error if the Space doesn't exist.
+* empty: Deletes all contents of a Space but does not delete the Space itself, leaving it empty. Generates an error if the Space does not exist.
+* sync_local_dir: Syncs content from a Space to a local directory. Generates an error if the Space does not exist; issuing a create action before this action will ensure that this does not occur.
+* sync_remote_dir: Syncs content from a local directory to a Space. Generates an error if the Space does not exist; issuing a create action before this action will ensure that this does not occur.
 
 ## Properties
 
 #### Required:
-* space: The name of the DigitalOcean Space. Can also be specified as the name of the resource for convenience.
-* local_path: The local path to sync files from/to.
+* space: The name of the Space. Can also be specified as the name of the resource for convenience.
 * key: A valid Spaces API key for the relevant DigitalOcean account.
 * secret_key: The secret key that matches the above key.
-* region: The DigitalOcean data center the Space is located in.
 
 #### Optional:
+* region: The DigitalOcean data center the Space is located in. Defaults to nyc3.
+* local_path: The local path to sync files from/to. Defaults to /do_space.
 * remote_path: The remote path on the Space to sync files from/to. Defaults to the root directory of the space.
+* delete_removed: Whether or not to remove files at the destination that no longer exist at the source. Defaults to false.
+* public_space: Whether or not to make a Space public when using the create or update action. Defaults to nil but can be set to true, false, or nil.
 
 ## What is this thing doing "under the hood"?
 
--- resources/do_space.rb
+++ resources/do_space.rb
@@ -1,25 +1,125 @@
 resource_name :do_space
 
 property :space, String, name_property: true
-property :local_path, String, required: true
+property :local_path, String, default: '/do_space'
 property :remote_path, String, default: ''
 property :key, String, required: true
 property :secret_key, String, required: true
-property :region, String, required: true
+property :region, String, default: 'nyc3'
+property :delete_removed, [true, false], default: false
+property :public_space, [true, false, nil], default: nil
 
-actions :sync_local_dir, :sync_remote_dir
-default_action :sync_local_dir
+actions :sync_local_dir, :sync_remote_dir, :create, :delete, :update, :empty
+default_action :create
 
 action_class do
   include DOSpacesCustomResource::Helpers
+end
+
+action :create do
+  local_config = node['do_space_resource']['s3cfg_local_path']
+  create_command = "s3cmd -c '#{local_config}' mb s3://#{new_resource.space}"
+  perm_command = "s3cmd -c '#{local_config}' setacl s3://#{new_resource.space}"
+
+  perm_command += if new_resource.public_space
+                    ' --acl-public'
+                  else
+                    ' --acl-private'
+                  end
+
+  s3cmd_package 'install s3cmd'
+
+  template local_config do
+    cookbook 'do-spaces'
+    source 's3cfg.erb'
+    variables(
+      key: new_resource.key,
+      secret_key: new_resource.secret_key,
+      region: new_resource.region
+    )
+  end
+
+  execute create_command do
+    returns [0, 13]
+  end
+
+  execute perm_command unless new_resource.public_space.nil?
+end
+
+action :delete do
+  local_config = node['do_space_resource']['s3cfg_local_path']
+  delete_command = "s3cmd -c '#{local_config}' rb s3://#{new_resource.space}"
+
+  s3cmd_package 'install s3cmd'
+
+  template local_config do
+    cookbook 'do-spaces'
+    source 's3cfg.erb'
+    variables(
+      key: new_resource.key,
+      secret_key: new_resource.secret_key,
+      region: new_resource.region
+    )
+  end
+
+  execute delete_command do
+    returns [0, 12]
+  end
+end
+
+action :empty do
+  local_config = node['do_space_resource']['s3cfg_local_path']
+  empty_command = "s3cmd -c '#{local_config}' rm s3://#{new_resource.space}/"\
+                  ' --recursive --force'
+
+  s3cmd_package 'install s3cmd'
+
+  template local_config do
+    cookbook 'do-spaces'
+    source 's3cfg.erb'
+    variables(
+      key: new_resource.key,
+      secret_key: new_resource.secret_key,
+      region: new_resource.region
+    )
+  end
+
+  execute empty_command
+end
+
+action :update do
+  local_config = node['do_space_resource']['s3cfg_local_path']
+  perm_command = "s3cmd -c '#{local_config}' setacl s3://#{new_resource.space}"
+
+  perm_command += if new_resource.public_space
+                    ' --acl-public'
+                  else
+                    ' --acl-private'
+                  end
+
+  s3cmd_package 'install s3cmd'
+
+  template local_config do
+    cookbook 'do-spaces'
+    source 's3cfg.erb'
+    variables(
+      key: new_resource.key,
+      secret_key: new_resource.secret_key,
+      region: new_resource.region
+    )
+  end
+
+  execute perm_command unless new_resource.public_space.nil?
 end
 
 action :sync_local_dir do
   local_config = node['do_space_resource']['s3cfg_local_path']
   local_path = local_path_sanitize(new_resource.local_path)
   remote_path = remote_path_sanitize(new_resource.remote_path)
-  sync_command = "s3cmd -c '#{local_config}' --delete-removed sync "\
-                 "s3://#{new_resource.space}#{remote_path} #{local_path}"
+
+  sync_command = "s3cmd -c '#{local_config}' "
+  sync_command += '--delete-removed ' if new_resource.delete_removed
+  sync_command += "sync s3://#{new_resource.space}#{remote_path} #{local_path}"
 
   s3cmd_package 'install s3cmd'
 
@@ -42,8 +142,10 @@ action :sync_remote_dir do
   local_config = node['do_space_resource']['s3cfg_local_path']
   local_path = local_path_sanitize(new_resource.local_path)
   remote_path = remote_path_sanitize(new_resource.remote_path)
-  sync_command = "s3cmd -c '#{local_config}' --delete-removed sync "\
-                 "#{local_path} s3://#{new_resource.space}#{remote_path}"
+
+  sync_command = "s3cmd -c '#{local_config}' "
+  sync_command += '--delete-removed ' if new_resource.delete_removed
+  sync_command += "sync #{local_path} s3://#{new_resource.space}#{remote_path}"
 
   s3cmd_package 'install s3cmd'
 
